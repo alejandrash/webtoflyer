d3f839c629762a11ce0d95d856a234a5acbdbb9e
Ale <alejandra.h@inosur.com>
2018-01-31 15:01:17 -0300

    ajuste

--- demo/includes/config.php
+++ demo/includes/config.php
@@ -3,19 +3,7 @@
 	//LOCAL
 	$dbserver = "localhost";
 	$dbname = "wtf_demo";
-	$dbuser = "root";
-	$dbpassword = "mysql";
-
-	//ONLINE
-	//$dbserver = "192.168.0.192";
-	//$dbname = "marquez_flyer";
-	//$dbuser = "marquez_flyer";
-	//$dbpassword = "marquez20";
-
-    //ONLINE WTF
-	//$dbserver = "160.153.153.144";
-	//$dbname = "marquez_flyer";
-	//$dbuser = "marquez_flyer";
-	//$dbpassword = "marquez20";
+	$dbuser = "";
+	$dbpassword = "";
 
 ?>
